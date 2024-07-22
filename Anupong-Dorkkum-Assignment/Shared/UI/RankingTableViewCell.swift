e1b4adab38583c6e99ec7f0d6d42c6900eb9fb23
anupong <actionscript@outlook.com>
2024-07-22 18:31:11 +0700

    add ranking coin

---
+++ Anupong-Dorkkum-Assignment/Shared/UI/RankingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RankingTableViewCell.swift
+//  Anupong-Dorkkum-Assignment
+//
+//  Created by gone on 22/7/2567 BE.
+//
+
+import UIKit
+
+class RankingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
